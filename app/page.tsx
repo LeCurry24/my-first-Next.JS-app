82e17228f36af3442b9af856ed5eb66b72d21ef7
jaylen curry <curryjaylen24@gmail.com>
2024-12-18 23:38:18 -0500

    just playing around with Nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,31 @@
-
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
+      <div className="navbar navbar-center bg-slate-700 rounded-md flex justify-between">
+        <details className="dropdown">
+          <summary className="btn m-2">open or close</summary>
+          <ul className="menu dropdown bg-base-100 ">
+            <li>
+              <Link href="/users">Users</Link>
+            </li>
+            <li>
+              <ProductCard />
+            </li>
+          </ul>
+        </details>
+        <div>
+        <input type="text" placeholder="Search" className="input w-full max-w-xs" />
+        </div>
+      </div>
       <h1>Hello world</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard/>
+      <p>How is your day?</p>
+      <div className="justify-self-center card-body rounded-md w-80 h-72 bg-sky-900 ">
+        <div className="border-2 h-40"></div>
+        <button className="btn btn-active btn-ghos mt-4">love</button>
+      </div>
     </main>
   );
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,27 +1,40 @@
 import React from "react";
-
-
+import AddToCart from "../components/AddToCart";
 
 interface User {
-    id: number;
-    name: string;
-    email: string;
+  id: number;
+  name: string;
+  email: string;
 }
 
 const UsersPage = async () => {
-   const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
-   const users: User[] = await res.json();
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  const users: User[] = await res.json();
 
-    return (
-        <>
-        <h1>Users</h1>
-        <p>{new Date().toLocaleTimeString()}</p>
-        <ul>
-            {users.map(user => <li key={user.id}>{user.name} <br /> <u>{user.email}</u></li> )}
-            
-        </ul>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <th>{user.name}</th><th>{user.email}</th><th><AddToCart/></th>
+            </tr>
+          ))}
+        </tbody>
+          
+      </table>
+    </>
+  );
+};
 
-export default UsersPage
+export default UsersPage;
